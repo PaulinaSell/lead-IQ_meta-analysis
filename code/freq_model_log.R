89f28231b99db6f46bb340104f09de87c28cf7d2
Paulina Sell <paulina.sell@gmail.com>
2026-09-04 17:24:20 +0200

    addressed R2C2 and some changes to R2C1

--- code/freq_model_log.R
+++ code/freq_model_log.R
@@ -2,9 +2,9 @@
 
 library(metafor)
 library(tidyverse)
-# library(distsfactory)
+library(distsfactory)
 
-data_full = read.csv("data/study_data_leadIQloss.csv")
+data_full <- read.csv("data/study_data_leadIQloss.csv")
 
 # run unadjusted model
 rma_model <- rma(
@@ -47,6 +47,8 @@ forest(rma_model_knha)
 
 # Parametric simulation of predictive intervals (hypothetical new studies)
 # 8000 draws to match the Bayesian analysis
+set.seed(8762)
+
 n_samples <- 8000
 mu_hat <- as.numeric(coef(rma_model_knha))
 se_mu <- rma_model_knha$se
@@ -54,25 +56,33 @@ tau_hat <- sqrt(rma_model_knha$tau2)
 se_tau <- rma_model_knha$se.tau2 / (2 * tau_hat)
 
 mu_samples <- rnorm(n_samples, mu_hat, se_mu)
-log_tau_mu <- log(tau_hat)
-log_tau_sd <- se_tau / tau_hat
-tau_samples <- exp(rnorm(n_samples, log_tau_mu, log_tau_sd))
-theta_new <- rnorm(n_samples, mu_samples, tau_samples)
 
-theta_new_mean <- mean(theta_new)
+# main predictive intervals (assuming trunc normal for tau)
+tau_alt <- make_dist(
+  "normal",
+  mean = tau_hat,
+  std = se_tau,
+  support = c(0, Inf)
+)
+tau_samples <- tau_alt$r(n_samples)
+theta_new <- rnorm(n_samples, mu_samples, tau_samples)
 theta_new_q025 <- quantile(theta_new, probs = 0.025)
 theta_new_q975 <- quantile(theta_new, probs = 0.975)
 
-tau_alt <- make_dist("normal", mean = tau_hat, std = se_tau, support = c(0, Inf))
-tau_alt_samples <- tau_alt$r(n_samples)
-theta_new_alt <- rnorm(n_samples, mu_samples, tau_alt_samples)
-theta_new_q025_alt <- quantile(theta_new_alt, probs = 0.025)
-theta_new_q975_alt <- quantile(theta_new_alt, probs = 0.975)
+# alternative predictive intervals (assuming log-normal for tau)
+log_tau_mu <- log(tau_hat)
+log_tau_sd <- se_tau / tau_hat
+tau_alt_samples <- exp(rnorm(n_samples, log_tau_mu, log_tau_sd))
+theta_new <- rnorm(n_samples, mu_samples, tau_alt_samples)
+
+theta_new_mean <- mean(theta_new)
+theta_new_q025_alt <- quantile(theta_new, probs = 0.025)
+theta_new_q975_alt <- quantile(theta_new, probs = 0.975)
 
 freq_ppi <- list(
   theta_new_mean = theta_new_mean,
   theta_new_q025 = theta_new_q025,
-  theta_new_q975 = theta_new_q975#,
+  theta_new_q975 = theta_new_q975,
   theta_new_q025_alt = theta_new_q025_alt, # -6.48
   theta_new_q975_alt = theta_new_q975_alt # 1.75
 )
